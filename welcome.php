10dc74603b096d4b70206b10f349fbf0f1e2a828
smmunna <64527538+smmunna@users.noreply.github.com>
2020-05-25 22:15:30 +0600

    Add files via upload
    Nothing to know..  I've made it..

---
+++ welcome.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>welcome</title>
+    <link rel="stylesheet" href="wel.css">
+</head>
+<body>
+  <div class="bod">
+Welcome <div class="x"> <?php echo $_POST['name'];?><br></div>
+   Your E-mail adress is:<div class="y"> <?php echo $_POST['email'];?><br></div>
+   And your age is: <div class="z"> <?php echo $_POST['age'];?></div>
+  
+<div class="p">1999-2020&copy munna </div>
+
+</div>
+</body>
+</html>
